ca08cc309b9d72e457cdabc7fbf49bf117cb3fce
Juan Mauro <jmauro492@gmail.com>
2022-03-08 16:29:10 -0300

    Add task. Delete task. Styles v 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listaJuan';
+  nombre = "Juan"
+
+
+  tasks : string[] = ["aaa" ,"asdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdsasdasdssasdasdsasdasdsasdasdsasdasds"];  
+  //tasks: string[] = [];
+
+  // Add Task
+  addTask(newTask: any) {
+    this.tasks.unshift(newTask);
+  }
+
+  // Edit Task
+  //editTask(task: any) {
+   // console.log("la va a editar", (this.tasks.indexOf(task)));
+   // this.tasks.indexOf((task), 1) = task;
+   //this.tasks.unshift(newTask);
+  //}
+
+
+  // Delete Task
+  deleteTask(task: any) {
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+    console.log(this.tasks);
+  }
+
+
 }
